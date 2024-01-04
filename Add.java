bfced8fc6794119add2f255baf25b4ab3529d117
pradyuprasad <pradyu.sp@gmail.com>
2024-01-04 18:01:30 +0530

    Addition with error catching

--- Add.java
+++ Add.java
@@ -1,11 +1,44 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Add {
 	public static void main (String[] args){
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("What's the first number??");
-		int number = scanner.nextLine();
-		System.out.println(number + 1);
-		scanner.close();
-	} 
+		int n1 = 0, n2 = 0;
+		boolean ValidInput = false;
+		while (!ValidInput){
+			try {
+				System.out.println("What is the first number?");
+				n1 = scanner.nextInt();
+				ValidInput = true;
+			}
+			catch (InputMismatchException ex){
+				System.out.println("That input was not permitted!");
+				scanner.next();
+			}
+			catch (Exception ex){
+				System.out.println("Error"+ ex);
+				scanner.next();
+			}
+		}
+		ValidInput = false;
+		while (!ValidInput){
+			try {
+				System.out.println("What is the second number?");
+				n2 = scanner.nextInt();
+				ValidInput = true;
+			}
+			catch (InputMismatchException ex){
+				System.out.println("That input was not permitted!");
+				scanner.next();
+			}
+			catch (Exception ex){
+				System.out.println("Error"+ ex);
+				scanner.next();
+			}
+		}
 
+		int sum = n1 + n2;
+
+		System.out.println("The sum of your two numbers is "+ sum);
+	}
 }
