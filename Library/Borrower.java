d27f1815c47ea3ec970b0b93410b592ca372b21c
pradyuprasad <pradyu.sp@gmail.com>
2024-01-06 19:54:01 +0530

    New data structure for booklist in borrower, add and remove functions in it done

--- Library/Borrower.java
+++ Library/Borrower.java
@@ -1,9 +1,36 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+
+class DuplicateBookException extends Exception{
+	public DuplicateBookException() {
+		super("This book has already been added!");
+	}
+}
+
+class BookNotThere extends Exception {
+	public BookNotThere(){
+		super("This book isn't there!");
+	}
+}
+
+
+
+/* 1. add list of books borrowed as an ArrayList of books DONE 
+ * 2. create add and remove functions to this DONE
+ * 3. Create view function also for this 
+ * */
+
+class NegativeException extends Exception{
+	public NegativeException(String message){
+		super(message);
+	}
+}
 public class Borrower{
 	private String Name;
 	private int BorrowerId;
 	private int BorrowCount;
 	private static int NextBorrowerId = 1;
+	private ArrayList<Book> BookList;
 
 	public static void main(String[] args){
 	}
@@ -36,9 +63,45 @@ public class Borrower{
 	public void IncreaseBorrow(){
 		this.BorrowCount += 1;
 	}
-	public void DecreaseBorrow(){
+	public void DecreaseBorrow() throws NegativeException{
 		if (this.BorrowCount <= 0){
-			throw 
+			throw new NegativeException("You have not borrowed any books!"); 
+		}
+		else {
+			this.BorrowCount -= 1;
 		}
 	}
+
+	public boolean CheckBook(Book b){
+		if (this.BookList.isEmpty()){
+			return false;
+		}
+		else {
+			for (Book k: this.BookList){
+				if (k.equals(b)){
+					return true;
+				}
+			}
+			return false;
+		}
+	}	
+
+	public void AddBook(Book b) throws DuplicateBookException{
+		if (CheckBook(b)){
+			throw new DuplicateBookException();
+		}
+		// check if b is already inside array
+		else {
+			this.BookList.add(b);
+		}
+	} 
+	public void RemoveBook(Book b) throws BookNotThere{
+		if (!CheckBook(b)){
+			throw new BookNotThere();
+		}
+		else {
+			this.BookList.remove(b);
+		}
+	} 
 }
+
--- Library/Library.java
+++ Library/Library.java
@@ -1,26 +1,58 @@
+class AlreadyBorrowed extends Exception {
+	public AlreadyBorrowed (){
+		super("This book has already been borrowed!");
+	}
+}
+
+
 public class Library{
 	public static void main(String[] args){
 		System.out.println("This is a library management system!");
-		Book b1 = new Book("The Power Broker", "1234", "Rober Caro");
+		Book b1 = new Book("The Power Broker", "1234", "Robert Caro");
 		Borrower pradyu = new Borrower("Pradyumna");
 	}
-	public static void Borrow(Book b, Borrower p){
-		if (b.GetBorrowed == true){
-			throw new 
+	public static void BorrowMethod(Book b, Borrower p) throws AlreadyBorrowed{
+		/* Steps 
+		 * 1. Check if book is borrowed
+		 * 2. Check if max borrow limit has been hit
+		 * Actual Borrowing
+		 * 1. Set borrowed to true
+		 * 1a. idk why this exception exists
+		 * 2. Set the borrower name to the name of the person borrowing
+		 * 3. Increase the person's borrow limit
+		 * 4. Put this into the person's borrowed books list (how???)
+		 *
+		 *
+		 *
+		 */
+		if (b.GetBorrowed() == true || b.GetBorrower() != ""){
+			// check if book is borrowed
+			throw new AlreadyBorrowed(); 
+			// if it is borrowed, throw exception that it is borrowed
 		}	
 		else {
 			try {
 			b.SetBorrowed(true);
 			}
-			catch (IllegalIllegalArgumentException ex) {
-				System.out.Println("Error!" + ex)
+			catch (IllegalArgumentException ex){
+				System.out.println("Error!" + ex);
 
 			}
-			b.SetBorrower(p.GetName());
-			p.
+			try{
+				b.SetBorrower(p.GetName());
+			}
+
+			catch (Exception ex){
+				System.out.println("Error!"+ ex);
+			}
+
+			try {
+				p.IncreaseBorrow();
+			}
+			catch (Exception ex){
+				System.out.println("Error!"+ ex);
+			}
 		}
 	}
-	
-	
 }
 
